e0c6656bd61f6d13a771cbdc4a883b3c362c1a55
Felipe <felipe_14_oliveira@hotmail.com>
2016-11-16 19:49:42 -0200

    g

---
+++ Fwk.Frm.Padrao.Principal.pas
@@ -0,0 +1,24 @@
+unit Fwk.Frm.Padrao.Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFwkFrmPadraoPrincipal = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FwkFrmPadraoPrincipal: TFwkFrmPadraoPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
